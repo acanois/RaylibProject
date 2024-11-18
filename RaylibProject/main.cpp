27bbecd392af8f94783bb1f8305960c1456055df
acanois <david.richter42@gmail.com>
2024-11-18 14:10:40 -0600

    Initial Commit

---
+++ RaylibProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RaylibProject
+//
+//  Created by David Richter on 11/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
